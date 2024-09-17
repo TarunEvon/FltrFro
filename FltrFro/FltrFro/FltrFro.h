734cae75c8de4f76327302f7b0d3ecc0fac4b3a6
TarunEvon <tarun.evontech@gmail.com>
2024-09-17 22:11:59 +0530

    add framework

---
+++ FltrFro/FltrFro/FltrFro.h
@@ -0,0 +1,18 @@
+//
+//  FltrFro.h
+//  FltrFro
+//
+//  Created by Tarun Kumar on 17/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FltrFro.
+FOUNDATION_EXPORT double FltrFroVersionNumber;
+
+//! Project version string for FltrFro.
+FOUNDATION_EXPORT const unsigned char FltrFroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FltrFro/PublicHeader.h>
+
+
